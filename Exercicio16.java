8c5f843407ba08e050185d629088f84fdd4c6c95
GabrielSCarloto <100328771+GabrielSCarloto@users.noreply.github.com>
2022-06-10 01:11:01 -0300

    Add files via upload

---
+++ Exercicio16.java
@@ -0,0 +1,80 @@
+package Exercicios_Java;
+
+import java.util.*;
+
+public class Exercicio16 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+				
+		int idade1;
+		int idade2;
+		int idade3;
+		int idade4;
+		int idade5;
+		int idade6;
+		int idade7;
+		int idade8;
+		int idade9;
+		int idade10;
+		int idade11;
+		int idade12;
+		int idade13;
+		int idade14;
+		int idade15;
+		int idade16;
+		int idade17;
+		int idade18;
+		int idade19;
+		int idade20;
+		int media;
+				
+		System.out.println("Você irá digitar a idade de 20 pessoas e no final saberá a média da soma dessas idades! \n");
+				
+		System.out.println("Digite o primeiro: \n");
+		idade1 = entrada.nextInt();
+		System.out.println("Digite o segundo: \n");
+		idade2 = entrada.nextInt();
+		System.out.println("Digite o terceiro: \n");
+		idade3 = entrada.nextInt();
+		System.out.println("Digite o quarto: \n");
+		idade4 = entrada.nextInt();
+		System.out.println("Digite o quinto: \n");
+		idade5 = entrada.nextInt();
+		System.out.println("Digite o sexto: \n");
+		idade6 = entrada.nextInt();
+		System.out.println("Digite o sétimo: \n");
+		idade7 = entrada.nextInt();
+		System.out.println("Digite o oitavo: \n");
+		idade8 = entrada.nextInt();
+		System.out.println("Digite o nono: \n");
+		idade9 = entrada.nextInt();
+		System.out.println("Digite o décimo: \n");
+		idade10 = entrada.nextInt();
+		System.out.println("Digite o décimo primeiro: \n");
+		idade11 = entrada.nextInt();
+		System.out.println("Digite o décimo segundo: \n");
+		idade12 = entrada.nextInt();
+		System.out.println("Digite o décimo terceiro: \n");
+		idade13 = entrada.nextInt();
+		System.out.println("Digite o décimo quarto: \n");
+		idade14 = entrada.nextInt();
+		System.out.println("Digite o décimo quinto: \n");
+		idade15 = entrada.nextInt();
+		System.out.println("Digite o décimo sexto: \n");
+		idade16 = entrada.nextInt();
+		System.out.println("Digite o décimo sétimo: \n");
+		idade17 = entrada.nextInt();
+		System.out.println("Digite o décimo oitavo: \n");
+		idade18 = entrada.nextInt();
+		System.out.println("Digite o décimo nono: \n");
+		idade19 = entrada.nextInt();
+		System.out.println("Digite o vigésimo: \n");
+		idade20 = entrada.nextInt();
+				
+		media = (idade1 + idade2 + idade3 + idade4 + idade5 + idade6 + idade7 + idade8 + idade9 + idade10 + idade11 + idade12 + idade13 + idade14 + idade15 + idade16 + idade17 + idade18 + idade19 + idade20) / 20;
+				
+		System.out.println("A média da soma desses números é: " + media);
+	}
+}
